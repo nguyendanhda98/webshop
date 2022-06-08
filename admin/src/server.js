3a1614e2b4e70ea89280a1f3a273ccd894c06c70
Nguyen Danh Da <nguyendanhda98@gmail.com>
2022-06-08 17:42:26 +0700

    feat(admin): add login

--- admin/src/server.js
+++ admin/src/server.js
@@ -5,11 +5,13 @@ const app = express();
 const port = 4000;
 
 const testRoute = require('./routes/test.route.js');
+const loginRoute = require('./routes/login.route.js');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use('/', testRoute);
+app.use('/login', loginRoute);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
