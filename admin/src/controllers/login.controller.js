a2d3341d59236cfb9bceeb17d33c21ad34e221f8
Nguyen Danh Da <nguyendanhda98@gmail.com>
2022-06-09 17:51:30 +0700

    feat(admin): login using jwt

--- admin/src/controllers/login.controller.js
+++ admin/src/controllers/login.controller.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
 const login = (req, res) => {
   res.render('login/login');
 };
@@ -5,7 +8,11 @@ const login = (req, res) => {
 const postLogin = (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  res.cookie('email', email);
+  const tokenSecret = process.env.ACCESS_TOKEN_SECRET;
+  const accessToken = jwt.sign({ email: email }, tokenSecret, {
+    expiresIn: '30s',
+  });
+  res.cookie('email', accessToken);
   res.redirect('/');
 };
 
