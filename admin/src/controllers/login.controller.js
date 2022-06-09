29eff247948f24f7288e06beeb67987285f38daf
Nguyen Danh Da <nguyendanhda98@gmail.com>
2022-06-09 10:02:02 +0700

    feat(admin): add login

--- admin/src/controllers/login.controller.js
+++ admin/src/controllers/login.controller.js
@@ -2,6 +2,10 @@ const login = (req, res) => {
   res.render('login/login');
 };
 
-const postLogin = (req, res) => {};
+const postLogin = (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  res.render('index', { name: email });
+};
 
 module.exports = { login, postLogin };
--- admin/src/server.js
+++ admin/src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const bodyParser = require('body-parser');
 const port = 4000;
 
 const testRoute = require('./routes/test.route.js');
@@ -9,6 +10,9 @@ const loginRoute = require('./routes/login.route.js');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.use('/', testRoute);
 app.use('/login', loginRoute);
